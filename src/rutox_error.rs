d6b18abf30a68a70ec8023e7d90b701f5673d252
Matheus Richard <matheusrichardt@gmail.com>
2022-02-15 11:31:32 -0300

    Add ProgrammerError

--- src/rutox_error.rs
+++ src/rutox_error.rs
@@ -1,6 +1,7 @@
 use super::scanner::src_location::SrcLocation;
 
 pub enum RutoxError {
+    ProgrammerError(String, SrcLocation),
     SyntaxError(String, SrcLocation),
 }
 
@@ -8,6 +9,10 @@ impl std::fmt::Display for RutoxError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             RutoxError::SyntaxError(msg, location) => write!(f, "SyntaxError: {msg} at {location}"),
+            RutoxError::ProgrammerError(msg, location) => write!(
+                f,
+                "ProgrammerError: {msg} at {location}.\nThis is a bug in rutox. Please report it."
+            ),
         }
     }
 }
