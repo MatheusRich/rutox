025c55df64e3dee1db18fb29965d98357e64955e
Matheus Richard <matheusrichardt@gmail.com>
2022-02-18 12:40:05 -0300

    Add code excerpt to error details

--- src/rutox_error.rs
+++ src/rutox_error.rs
@@ -59,38 +59,98 @@ impl RutoxError {
     }
 
     fn code_excerpt(&self, source_file_content: &str) -> String {
-        let code_column = format!(" {} |", self.location().line).blue().bold();
-        let empty_column = format!(" {} |", " ".repeat(self.location().line.to_string().len()))
-            .blue()
-            .bold();
+        let error_line_and_surroundings = self.extract_error_line_and_surroundings(source_file_content);
+        let biggest_line_num_digit_count = error_line_and_surroundings
+            .iter()
+            .last()
+            .expect("There should be at least one error line")
+            .0
+            .to_string()
+            .len();
 
-        let src_line = format!(
-            "{} {}",
-            code_column,
-            self.extract_error_line(source_file_content)
-        );
-        let error_indication = format!(
-            "{}{}{}",
-            empty_column,
-            " ".repeat(self.location().col),
-            "^ the error occurred here"
-        )
-        .yellow()
-        .bold();
-
+        let mut code_excerpt = String::new();
+        for (line, code, should_highlight) in error_line_and_surroundings {
+            code_excerpt.push_str(
+                &self
+                    .code_column(line, biggest_line_num_digit_count)
+                    .to_string(),
+            );
+            if should_highlight {
+                code_excerpt.push_str(&code.bold().white().to_string());
+            } else {
+                code_excerpt.push_str(&code);
+            }
+            if line == self.location().line as i64 {
+                code_excerpt.push('\n');
+                code_excerpt.push_str(
+                    &self
+                        .error_indication(biggest_line_num_digit_count)
+                        .to_string(),
+                );
+            }
+            code_excerpt.push('\n');
+        }
         format!(
-            "{}\n{}\n{}\n{}",
-            empty_column, src_line, error_indication, empty_column
+            "{}\n{}{}",
+            self.empty_column(biggest_line_num_digit_count),
+            code_excerpt,
+            self.empty_column(biggest_line_num_digit_count)
         )
     }
 
-    fn extract_error_line(&self, source_file_content: &str) -> String {
-        let error_line_index = self.location().line - 1;
+    fn code_column(&self, line_num: i64, biggest_line_num: usize) -> ColoredString {
+        let padding = " ".repeat(biggest_line_num - (line_num.to_string().len()));
+
+        format!(" {}{} | ", padding, line_num).blue().bold()
+    }
+
+    fn empty_column(&self, biggest_line_num: usize) -> ColoredString {
+        let padding = " ".repeat(biggest_line_num);
+
+        format!(" {} | ", padding).blue().bold()
+    }
+
+    fn error_indication(&self, biggest_line_num: usize) -> ColoredString {
+        let error_col = if self.location().col == 0 {
+            0
+        } else {
+            self.location().col - 1
+        };
+
+        format!(
+            "{}{}{}",
+            self.empty_column(biggest_line_num),
+            " ".repeat(error_col),
+            "^ the error occurred here"
+        )
+        .yellow()
+        .bold()
+    }
+
+    fn extract_error_line_and_surroundings(
+        &self,
+        source_file_content: &str,
+    ) -> Vec<(i64, String, bool)> {
+        let error_line_index = self.location().line as i64 - 1;
+        let line_before_error_index = error_line_index - 1;
+        let line_after_error_index = error_line_index + 1;
 
         source_file_content
             .split('\n')
-            .nth(error_line_index)
-            .expect("Could not find error line in source file.")
-            .to_string()
+            .map(|l| l.to_string())
+            .enumerate()
+            .map(|(i, line)| match i {
+                v if v as i64 == line_before_error_index => {
+                    (Some(line_before_error_index + 1), line, false)
+                }
+                v if v as i64 == error_line_index => (Some(error_line_index + 1), line, true),
+                v if v as i64 == line_after_error_index => {
+                    (Some(line_after_error_index + 1), line, false)
+                }
+                _ => (None, "".to_string(), false),
+            })
+            .filter(|(i, _, _)| i.is_some())
+            .map(|(i, line, should_highlight)| (i.unwrap(), line, should_highlight))
+            .collect()
     }
 }
